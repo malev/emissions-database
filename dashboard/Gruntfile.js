ab12480c2c2d693e3f9a6c81a9b5563458cdcab1
Marcos Vanetta <marcosvanetta@gmail.com>
2015-02-03 15:12:26 -0600

    JS builder

---
+++ dashboard/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/foundation/js/foundation.js',
+          'bower_components/chartjs/Chart.js'
+        ],
+        dest: '../emission_events/static/js/vendor.js',
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat']);
+
+};
